c018c017e848bb445bdf0c84bc094590c52c4380
bomb0069 <bomb0069@vm-0.bomb0069.koding.kd.io>
2014-04-08 16:25:45 +0000

    Refactor by Using Recursive

--- src/primes/primes.go
+++ src/primes/primes.go
@@ -6,12 +6,14 @@ func (primes Primes) GetPrimesFactor() (primesFactor []int) {
   if int(primes) == 1 {
 	primesFactor = []int{}	
   } else {
+    
     for testFactor := 2; testFactor <= int(primes); testFactor++ {
       canDivideByTestFactor := int(primes) % testFactor == 0
       if canDivideByTestFactor {
-          primesFactor = append(primesFactor,testFactor)
-          primes = Primes(int(primes) / testFactor)
-          testFactor--
+        newPrimes := Primes(int(primes) / testFactor)
+        primesFactor = append (primesFactor, testFactor)
+        primesFactor = append (primesFactor, newPrimes.GetPrimesFactor()...)
+        break;
       }
     }
   }
